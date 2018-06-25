0bd6a7f9489f7eef8a03ab7f4faa102f866133f6
Henry Chen <tahafut@gmail.com>
2018-06-24 21:22:28 -0700

    new post

---
+++ 2018/06/24/enum3.py
@@ -0,0 +1,19 @@
+class AutoDict(dict):
+    def __init__(self):
+        self.count = 0
+
+    def __getitem__(self, key):
+        if key.startswith("__") and key.endswith("__"):
+            return super().__getitem__(key)
+
+        self[key] = self.count
+        self.count += 1
+
+
+class EnumMeta(type):
+    def __prepare__(*args):
+        return AutoDict()
+
+
+class Enum(metaclass=EnumMeta):
+    pass
---
+++ 2018/06/24/test_enum3.py
@@ -0,0 +1,18 @@
+"""
+>>> from enum3 import Enum
+
+>>> class Color(Enum):
+...     RED
+...     GREEN
+...     BLUE
+
+>>> Color.RED
+0
+
+>>> Color.GREEN
+1
+
+>>> Color.BLUE
+2
+
+"""
